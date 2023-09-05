2b28ec6ad66d4fe6d260c878440f9d99b6a19c09
Kisly93 <spsvk.kis.sov@gmail.com>
2023-09-05 20:04:26 +0200

    added verbose_name

--- places/models.py
+++ places/models.py
@@ -13,7 +13,7 @@ class Place(models.Model):
         return self.title
 
 class Image(models.Model):
-    location = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
+    location = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='Локация', related_name='images')
     image = models.ImageField(verbose_name='Картинка', upload_to='media/')
     order = models.PositiveIntegerField(verbose_name='Порядок', default=0)
 
